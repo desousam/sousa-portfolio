a055baff068e83c42d520e8a267cbfd1072735b9
de Sousa <mariliacs0239@gmail.com>
2026-01-16 14:44:25 -0300

    Update Navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,12 +5,12 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-        <nav>
-            <a href="">
-                <Image src={assets.logo} className='w-28 cursor-pointer mr-14'/>
+        <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+            <a href="#top">
+                <Image src={assets.logo} alt='' className='w-28 cursor-pointer mr-14'/>
             </a>
 
-            <ul>
+            <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
                 <li><a href="#top">Home</a></li>    
                 <li><a href="#about">Sobre mim</a></li>
                 <li><a href="#services">Serviços</a></li>
@@ -19,7 +19,7 @@ const Navbar = () => {
             </ul>
 
             <div>
-                <a href="#contact">Contato <Image src={assets.arrow_icon} className='w-3'/></a>
+                <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4'>Contato <Image src={assets.arrow_icon} alt='' className='w-3'/></a>
             </div>
         </nav>
     </>
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,6 +2,18 @@ const config = {
   plugins: {
     "@tailwindcss/postcss": {},
   },
+  theme: {
+    extend: {
+      colors: {
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme: '#11001f',
+      },
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+      },
+    },
+  },
 };
 
 export default config;
