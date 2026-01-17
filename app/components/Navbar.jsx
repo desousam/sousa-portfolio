856ec1fd03999f8bd44a85200accc99bd16662e2
de Sousa <mariliacs0239@gmail.com>
2026-01-17 09:49:02 -0300

    Update Navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,18 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Navbar = () => {
+
+    const sideMenuRef = useRef();
+
+    const openMenu = ()=>{
+        sideMenuRef.current.style.transform = 'translateX(-16rem)'
+    }
+    const closeMenu = ()=>{
+        sideMenuRef.current.style.transform = 'translateX(16rem)'
+    }
+
   return (
     <>
         <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
@@ -26,22 +36,22 @@ const Navbar = () => {
                     <Image src={assets.moon_icon} alt='' className='w-6'/>
                 </button>
                 <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4'>Contato <Image src={assets.arrow_icon} alt='' className='w-3'/></a>
-                <button className='block md:hidden ml-3'>
+                <button className='block md:hidden ml-3' onClick={openMenu}>
                     <Image src={assets.menu_black} alt='' className='w-6'/>
                 </button>
             </div>
 
             {/* -- ----- mobile menu ----- -- */}
 
-            <ul className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
-                <div className='absolute right-6 top-6'>
+            <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+                <div className='absolute right-6 top-6' onClick={closeMenu}>
                     <Image src={assets.close_black} alt='' className='w-5 cursor-pointer'/>
                 </div>
-                <li><a href="#top">Home</a></li>    
-                <li><a href="#about">Sobre mim</a></li>
-                <li><a href="#services">Serviços</a></li>
-                <li><a href="#work">Trabalho</a></li>
-                <li><a href="#contact">Contato</a></li>
+                <li><a onClick={closeMenu} href="#top">Home</a></li>    
+                <li><a onClick={closeMenu} href="#about">Sobre mim</a></li>
+                <li><a onClick={closeMenu} href="#services">Serviços</a></li>
+                <li><a onClick={closeMenu} href="#work">Trabalho</a></li>
+                <li><a onClick={closeMenu} href="#contact">Contato</a></li>
             </ul>
         </nav>
     </>
