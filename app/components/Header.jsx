f761b0f46628284269b2c1c6b4644dd122e6ac5c
de Sousa <mariliacs0239@gmail.com>
2026-01-17 10:47:44 -0300

    Create Header

---
+++ app/components/Header.jsx
@@ -0,0 +1,23 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
+      </div>
+        <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3'>Hi! I'm Marilia Sousa <Image src={assets.hand_icon} alt='' className='w-6'/></h3>
+        <h1 className='text-3xl sm:text-6xl lg:text-[66px]'>frontend developer & web designer based in Brazil.</h1>
+        <p className='max-w-2xl mx-auto'>Sou uma desenvolvedora em formação, focada em criar interfaces simples, funcionais e com boa experiência para o usuário. Estou aprendendo, evoluindo e construindo projetos todos os dias.</p>
+
+        <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+            <a href="#contact" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'>contato <Image src={assets.right_arrow_white} alt='' className='w-4'/></a>
+            <a href="/Marilia-Sousa-TI-Curriculo.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>currículo <Image src={assets.download_icon} alt='' className='w-4'/></a>
+        </div>
+    </div>
+  )
+}
+
+export default Header
