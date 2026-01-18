775be0399a23ec2a12cb164c88b0b240dc3da729
de Sousa <mariliacs0239@gmail.com>
2026-01-18 00:58:49 -0300

    Update Work

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -9,7 +9,7 @@ const Work = () => {
       <h2 className='text-center text-5xl'>Meus últimos trabalhos</h2>
       <p className='text-center max-w-2xl mx-auto mt-5 mb-12'>Bem-vinda(o) ao meu portfólio de desenvolvimento web. Aqui você encontra uma seleção de projetos que representam minha evolução no frontend, design e experiência do usuário.</p>
 
-      <div className='grid grid-cols-auto my-10 gap-5'>
+      <div className='grid grid-cols-4 my-10 gap-5'>
         {workData.map((project, index)=>(
             <div key={index} className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group' style={{backgroundImage: `url(${project.bgImage})`}}>
                 <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
