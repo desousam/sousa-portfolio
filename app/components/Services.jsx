f3799e4148e98669b1194bcd7f6fca82b312668c
de Sousa <mariliacs0239@gmail.com>
2026-01-17 22:41:59 -0300

    Update Services

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,4 +1,4 @@
-import { serviceData } from '@/assets/assets'
+import { assets, serviceData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
@@ -11,11 +11,11 @@ const Services = () => {
 
       <div className='grid grid-cols-4 gap-6 my-10'>
         {serviceData.map(({icon, title, description, link}, index)=>(
-            <div key={index}>
+            <div key={index} className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:lightHover hover:-translate-y-1 duration-500'>
                 <Image src={icon} alt='' className='w-10'/>
                 <h3 className='text-lg my-4 text-gray-700'>{title}</h3>
                 <p className='text-sm text-gray-600 leading-5'>{description}</p>
-                <a href="{link}" className='flex '>Leia mais</a>
+                <a href="{link}" className='flex items-center gap-2 text-sm mt-5'>Leia mais <Image src={assets.right_arrow} alt='' className='w-4'/></a>
             </div>
         ))}
       </div>
