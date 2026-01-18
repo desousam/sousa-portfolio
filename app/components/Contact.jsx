96064adfbddf7e7940cf4826eac24836d7712ed5
de Sousa <mariliacs0239@gmail.com>
2026-01-17 23:57:38 -0300

    Create Contact

---
+++ app/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center'>
+      <h4 className='text-center mb-2 text-lg'>Conecte-se comigo</h4>
+      <h2 className='text-center text-5xl'>Entre em contato</h2>
+      <p className='text-center max-w-2xl mx-auto mt-5 mb-12'>Ficarei feliz em conversar! Se você tiver alguma dúvida, proposta ou quiser trocar ideias sobre projetos, é só me enviar uma mensagem pelo formulário abaixo.</p>
+    </div>
+  )
+}
+
+export default Contact
